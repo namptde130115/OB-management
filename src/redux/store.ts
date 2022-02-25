46af993316722a18ba4ab6e7f097a76e3cd633a5
namptde130115 <44977870+namptde130115@users.noreply.github.com>
2022-02-25 09:56:41 +0700

    setup saga

--- src/redux/store.ts
+++ src/redux/store.ts
@@ -1,11 +1,20 @@
 import { configureStore, ThunkAction, Action } from '@reduxjs/toolkit';
 import counterReducer from '../features/counter/counterSlice';
+import createSagaMiddleware from '@redux-saga/core';
+import rootSaga from './rootSaga';
 
+const middlewareSaga = createSagaMiddleware();
 export const store = configureStore({
   reducer: {
     counter: counterReducer,
   },
+
+  // khai bao saga middleware
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware().concat(middlewareSaga),
 });
+
+middlewareSaga.run(rootSaga);
 
 export type AppDispatch = typeof store.dispatch;
 export type RootState = ReturnType<typeof store.getState>;
